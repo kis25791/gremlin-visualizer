c1bd78832bbbc36d878274b8105aeeaa53965e14
Kayel <kayel.lethanh@gmail.com>
2020-04-16 12:36:56 +0800

    Init commit

---
+++ build/precache-manifest.6ef11bac8c370d46973a3dfed2d5671a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395051e8983e8ae440e718fb64a4953a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8572bb02d2f947591b3b",
+    "url": "/static/css/2.8c1f6bf6.chunk.css"
+  },
+  {
+    "revision": "8572bb02d2f947591b3b",
+    "url": "/static/js/2.e7103c01.chunk.js"
+  },
+  {
+    "revision": "24a35506af3f5dd3b5afc46c443d823a",
+    "url": "/static/js/2.e7103c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7943626ee17bc5e5388",
+    "url": "/static/js/main.c497cb47.chunk.js"
+  },
+  {
+    "revision": "253b75fb2cf9070af55c",
+    "url": "/static/js/runtime-main.a31ccfaf.js"
+  }
+]);
